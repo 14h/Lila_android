9989f86cdfe2a932af053be9ae7bb6060815559a
ayhom3 <aeham.aeham44@gmail.com>
2023-02-15 12:48:23 +0100

    add firgun activity and navigate to it by clicking on Firgun view

---
+++ app/src/main/java/com/underscorevoid/lila/FirgunActivity.kt
@@ -0,0 +1,11 @@
+package com.underscorevoid.lila
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirgunActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firgun)
+    }
+}
--- app/src/main/java/com/underscorevoid/lila/MainActivity.kt
+++ app/src/main/java/com/underscorevoid/lila/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.underscorevoid.lila
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firgunBtn : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        firgunBtn = findViewById(R.id.firgunBtn);
+        firgunBtn.setOnClickListener {
+            val intent = Intent(this, FirgunActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
+
 }
