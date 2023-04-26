849ef4b53b254b801a17c6f29a2d909e4c69e668
Aeham Alhomsi <aehamalhomsi@Aehams-MacBook-Pro.local>
2023-04-26 22:08:50 +0200

    check that sexy slider

--- app/src/main/java/com/underscorevoid/lila/FirgunActivity.kt
+++ app/src/main/java/com/underscorevoid/lila/FirgunActivity.kt
@@ -2,6 +2,8 @@ package com.underscorevoid.lila
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.SeekBar
+
 
 class FirgunActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +11,18 @@ class FirgunActivity: AppCompatActivity() {
         setContentView(R.layout.firgun)
     }
 }
+class MyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firgun)
+
+        val slider = SeekBar(this)
+        // Customize the slider...
+//        firgun_slider.addView(slider)
+    }
+}
 
 
 
-// change1
-// change2
+
